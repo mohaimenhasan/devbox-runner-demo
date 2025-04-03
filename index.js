12276e05bc40e4f9461860fab03e85cdb80ffdf1
Mohaimen Khan <mohaimenkhan@microsoft.com>
2025-04-03 14:42:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello from DevBox Runner Demo!' });
 });
 
+// adding a comment here 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 }); 
